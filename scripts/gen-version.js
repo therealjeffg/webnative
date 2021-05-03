41f97cdff7bf9103ca2066dc96d2077ba442f9f4
Philipp Krüger <philipp.krueger1@gmail.com>
2021-05-03 15:07:51 +0200

    Generate the expected type for `VERSION`
    instead of assuming it's a string.

--- scripts/gen-version.js
+++ scripts/gen-version.js
@@ -3,7 +3,7 @@ const fs = require("fs").promises
 fs.readFile("package.json", { encoding: "utf-8" }).then(pkg => {
     const pkgJson = JSON.parse(pkg)
     const version = pkgJson.version
-    const versionModule = `export const VERSION = ${JSON.stringify(version, null, 4)}` + "\n"
+    const versionModule = `export const VERSION: string = ${JSON.stringify(version, null, 4)}` + "\n"
     return fs.writeFile("src/common/version.ts", versionModule, { encoding: "utf-8" })
 }).catch(e => {
     console.error(`There was an issue while trying to generate a "src/common/version.ts" file: ${e.message}\n${e}`)
