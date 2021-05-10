7b7f943fb9249174ae7d76beaec2edb921069dde
Philipp Krüger <philipp.krueger1@gmail.com>
2021-05-10 21:02:16 +0200

    Switch to using Uint8Arrays for file content

--- tests/fs/keystore-node.test.ts
+++ tests/fs/keystore-node.test.ts
@@ -1,6 +1,8 @@
+import cbor from 'borc'
 import crypto from "crypto"
 import * as aes from "keystore-idb/aes/index"
 import { SymmKeyLength } from "keystore-idb/types"
+import { NODE_IMPLEMENTATION } from "../setup-node"
 
 // @ts-ignore
 globalThis.crypto = crypto.webcrypto
@@ -9,9 +11,16 @@ globalThis.crypto = crypto.webcrypto
 describe("aes", () => {
   it("correctly encrypts & decrypts again", async () => {
     const key = await aes.makeKey({ length: SymmKeyLength.B256 })
-    const message = "Hello, World!"
-    const cipher = await aes.encrypt(message, key)
-    const decipher = await aes.decrypt(cipher, key)
-    expect(decipher).toEqual(message)
+    const keyStr = await aes.exportKey(key)
+
+    const message = {
+      hello: "world!"
+    }
+    const encoded: Uint8Array = Uint8Array.from(cbor.encode(message))
+    const cipher: Uint8Array = await NODE_IMPLEMENTATION.aes.encrypt(encoded, keyStr)
+    const decipher: Uint8Array = await NODE_IMPLEMENTATION.aes.decrypt(cipher, keyStr)
+    const decoded = cbor.decode(Buffer.from(decipher))
+
+    expect(decoded).toEqual(message)
   })
 })
